64e08d708ff177c3f75ebbb80b80775626fd5c60
Andreaperez1 <ap852892@gmail.com>
2024-07-03 15:59:25 -0500

    vista principal

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,87 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-card
+    class="mx-auto border-md rounded-lg card"
+    height="80%"
+    width="40%"
+    rounded="0"
+    flat
+  >
+    <v-toolbar
+      extended
+      prominent
+      class="bar"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-card-text>
+        <v-text-field
+          :loading="loading"
+          append-inner-icon="mdi-magnify"
+          density="compact"
+          label="Search templates"
+          variant="solo"
+          hide-details
+          single-line
+          @click:append-inner="onClick"
+        ></v-text-field>
+      </v-card-text>
+    </v-toolbar>
+
+    <v-card class="mx-auto my-15 card2" max-width="444">
+      <v-card-text>
+        <v-carousel
+    v-model="activeSlide"
+    :cycle="true"
+    :interval="5000"
+    height="400"
+    hide-delimiters
+    show-arrows="false"
+  >
+    <v-carousel-item v-for="(item, index) in items" :key="index">
+      <v-img :src="require(`@/assets/${item.src}`)" :alt="item.alt" height="400"></v-img>
+    </v-carousel-item>
+  </v-carousel>
+      </v-card-text>
+    </v-card>
+  </v-card>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      loaded: false,
+      loading: false,
+      activeSlide: 1, // Índice de la diapositiva activa
+      items: [
+        { src: 'adele.jpg', alt: 'Imagen 1' },
+        { src: 'rihana.jpg', alt: 'Imagen 2' },
+        // Agrega más objetos según sea necesario
+      ]
+    };
+  },
+
+  methods: {
+    onClick() {
+      this.loading = true;
+      setTimeout(() => {
+        this.loading = false;
+        this.loaded = true;
+      }, 2000);
+    }
   }
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.card {
+  margin-top: 20px;
+  background: radial-gradient(circle, rgba(238, 174, 202, 1) 0%, rgba(137, 41, 184, 1) 100%);
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.bar {
+  background: radial-gradient(circle, rgba(238, 174, 202, 1) 0%, rgba(137, 41, 184, 1) 100%);
+  height: 5%;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.card2 {
+  margin-top: 20px;
 }
 </style>
